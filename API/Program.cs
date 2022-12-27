aa46612fd67f8b57ea18f609390129681444c21c
hatsanai sungnark <54577966+kongsanais@users.noreply.github.com>
2022-12-27 13:55:20 +0700

    update font

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+
 // Configure http pipline
-app.UseAuthorization();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
-
 app.Run();
